9d3225342f4ecd76016cc49838d3f3bf46d256f9
nickdraper <nickdraper8@gmail.com>
2020-10-05 07:08:33 -0400

    initial commit to github

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const db = require('./config/keys').mongoURI;
+const mongoose = require('mongoose');
+const users = require("./routes/api/users");
+
+mongoose
+.connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
+
+
+app.get("/", (req, res) => res.send("Bread"));
+
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
