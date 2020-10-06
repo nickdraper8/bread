818994529f20f7fc70ddc324bcc087f8d5003e84
drewshroyer <drewshroyer@gmail.com>
2020-10-06 14:11:13 -0400

    frontend changes

--- frontend/src/components/user-home/user-home.jsx
+++ frontend/src/components/user-home/user-home.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import InternalNavbar from "../nav/navbar";
 import FriendsIndexItem from "./friends-index-item";
 import UserEventItem from "./user-event-item";
+import UserSearch from "../search/user_search"
 import "./user-home.css";
 
 class UserHome extends React.Component {
@@ -13,22 +14,20 @@ class UserHome extends React.Component {
   render() {
     return (
       <div className="user-home-container">
-       
-          <div className="left-user-home-container">
-            <div class="user-home-internal-title">BREAD</div>
-            {/* <SearchBar /> */}
-            <FriendsIndexItem />
-          </div>
+        <div className="left-user-home-container">
+          <div className="user-home-internal-title">BREAD</div>
+          <UserSearch />
+          <FriendsIndexItem />
+        </div>
 
-          <div className="right-user-home-container">
-            <InternalNavbar />
-            <ul className="user-event-tiles">
-              {/* {events.map((event) => ( */}
-              <UserEventItem />
-              {/* ))} */}
-            </ul>
-          </div>
-      
+        <div className="right-user-home-container">
+          <InternalNavbar />
+          <ul className="user-event-tiles">
+            {/* {events.map((event) => ( */}
+            <UserEventItem />
+            {/* ))} */}
+          </ul>
+        </div>
       </div>
     );
   }
