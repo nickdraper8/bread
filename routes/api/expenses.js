2c4852fa3b0f34461650c996f7b6f10571e09516
“Ravneet <your_email_here>
2020-10-08 17:24:45 -0400

    expenses

--- routes/api/expenses.js
+++ routes/api/expenses.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 router.get("/:userId", (req, res) => {
 
   Expense.find({ id: mongoose.ObjectId(req.params.userId) })
-    .select("amount date description payer_id")
+    .select("amount date")
     .then((expense) => {
       return res.json(expense);
     });
@@ -20,22 +20,21 @@ router.get("/", (req, res) => {
       return ({
         price: expenses.amount,
         date: expenses.date,
-        id: expenses.id,
-        payer_id: expenses.payer_id
+        id: expenses.id
       })
     }));
   });
 });
 
+
 router.post("/new", (req, res) => {
   const newExpense = new Expense({
-    description: req.body.description,
     amount: req.body.amount,
-    payer_id: req.body.payer_id,
-    event_id: req.body.event_id
+    payer_id: req.body.userId,
   });
   newExpense.save().then((expenses) => res.json(expenses));
 });
+
 
 router.delete("/delete/:expenseId", (req, res) => {
   Expense.findByIdAndRemove(req.params.expenseId)
@@ -44,33 +43,12 @@ router.delete("/delete/:expenseId", (req, res) => {
         return res.status(404).send({
           message: "Expenses not found",
         });
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 5c4d43815e6959922d1e04bf9d3798bc69583a2a
       }
       return res.send({ message: "Expenses successfully deleted" });
     })
     .catch((err) => {
       res.status(400).send({ message: "Could not delete expense" });
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 5c4d43815e6959922d1e04bf9d3798bc69583a2a
-        newExpense.save().then((expenses) => res.json(expenses));
     });
 });
 
-<<<<<<< HEAD
-router.get("/:id/expense", (req, res) => {
-  Expense.find({ id: mongoose.ObjectId(req.params.id) })
-    .select("price")
-    .then((expense) => {
-      return res.json(expense);
->>>>>>> f57f3b561daf4404aa61c4c70eceb00f7bfa6071
-    });
-});
-
-=======
->>>>>>> 5c4d43815e6959922d1e04bf9d3798bc69583a2a
 module.exports = router;
