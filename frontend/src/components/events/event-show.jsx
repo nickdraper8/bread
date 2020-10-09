b9ca4fdc1c1ef06a51d2c379f04dbcf7f45e5da6
nickdraper <nickdraper8@gmail.com>
2020-10-09 12:21:31 -0400

    took out some debuggers, commented lines of code

--- frontend/src/components/events/event-show.jsx
+++ frontend/src/components/events/event-show.jsx
@@ -18,17 +18,10 @@ class EventShow extends React.Component {
 
   render() {
     const { event } = this.props;
-    let attendees = Object.values(this.props.attendees).map(attendee => {
-      return <li>{attendee.firstname}</li>
-    });
-    debugger
+
     return (
       <div className="event-show-page-container" onClick={this.handleRedirect}>
         <div className="event-show-page-title">{event.name}</div>
-        {/* <div className="event-show-page-attendees"> */}
-          {/* <div className="event-show-page-attendees-title">Attendees</div>
-          {attendees}
-        </div> */}
         <div className="event-details-button">Event Details</div>
       </div>
     );
